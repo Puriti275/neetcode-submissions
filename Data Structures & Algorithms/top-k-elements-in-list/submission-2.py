d028b2eb2182d3db32fa4323f7050f164f161787
Andrew Ellis <puriti275@gmail.com>
2026-05-15 00:36:39 -0500

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        Input: List[int]
+        Output: List[int]
+
+        Methodology: iterate through the list and add frequencies to a
+        hashmap. The key is the number, and the value is the frequency
+
+        You're going to return the k most frequent elements. To do this,
+        we can sort the hashmap by the greatest values, and add them to
+        the result list. Afterward, we return it.
+
+        edge cases:
+        1. empty array
+        2. 
+        """
+
+        result = []
+        hash = defaultdict(int)
+
+        for num in nums:
+            hash[num] += 1
+
+        sorted_hash = dict(sorted(hash.items(), key=lambda item: item[1], reverse=True))
+        sorted1 = list(sorted_hash)
+
+        for i in range(k):
+            result.append(sorted1[i])
+
+        return result
